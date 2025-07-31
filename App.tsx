486398fcce416c6b80e36dbc0ecce6077a7fbabe
Parameswaran8 <paramsir8@gmail.com>
2025-07-31 22:45:14 +0530

    Testing purpose

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const App = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>App Hello user</Text>
     </View>
   );
 };
